4aeb45015b547f3ca61dbe25d3241dadd296a873
MEEP of Faith <meepmatthew@gmail.com>
2022-12-16 11:39:15 -0800

    Copy code over from PM

--- src/tranqol/content/TranQOLBlocks.java
+++ src/tranqol/content/TranQOLBlocks.java
@@ -1,6 +1,12 @@
 package tranqol.content;
 
+import mindustry.content.*;
+import mindustry.type.*;
 import mindustry.world.*;
+import tranqol.world.blocks.liquid.*;
+import tranqol.world.blocks.power.*;
+
+import static mindustry.type.ItemStack.*;
 
 public class TranQOLBlocks{
     public static Block
@@ -23,17 +29,54 @@ public class TranQOLBlocks{
     public static void load(){
         // region liquid
 
-        //TODO
+        liquidOverflowGate = new LiquidOverflowGate("liquid-overflow-gate"){{
+            requirements(Category.liquid, with(Items.graphite, 6, Items.metaglass, 10));
+            solid = false;
+            underBullets = true;
+        }};
+
+        liquidUnderflowGate = new LiquidOverflowGate("liquid-underflow-gate"){{
+            requirements(Category.liquid, with(Items.graphite, 6, Items.metaglass, 10));
+            solid = false;
+            underBullets = true;
+            invert = true;
+        }};
 
         // endregion
         // region liquid - Erekir
 
-        //TODO
+        reinforcedLiquidOverflowGate = new LiquidOverflowGate("reinforced-liquid-overflow-gate"){{
+            requirements(Category.liquid, with(Items.graphite, 6, Items.beryllium, 10));
+            buildCostMultiplier = 3f;
+            health = 260;
+            researchCostMultiplier = 1;
+            solid = false;
+            underBullets = true;
+        }};
+
+        reinforcedLiquidUnderflowGate = new LiquidOverflowGate("reinforced-liquid-underflow-gate"){{
+            requirements(Category.liquid, with(Items.graphite, 6, Items.beryllium, 10));
+            buildCostMultiplier = 3f;
+            health = 260;
+            researchCostMultiplier = 1;
+            invert = true;
+            solid = false;
+            underBullets = true;
+        }};
 
         // endregion
         // region power -
 
-        //TODO
+        beamDiode = new BeamDiode("beam-diode"){{
+            requirements(Category.power, with(
+                Items.beryllium, 10,
+                Items.silicon, 10,
+                Items.surgeAlloy, 5
+            ));
+            health = 90;
+            range = 10;
+            fogRadius = 1;
+        }};
 
         //endregion
     }
