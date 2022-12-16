01ca20029bb8ece152155dffddc210fe251e76df
MEEP of Faith <meepmatthew@gmail.com>
2022-12-16 11:35:31 -0800

    More setup

---
+++ src/tranqol/TranQOL.java
@@ -0,0 +1,14 @@
+package tranqol;
+
+import mindustry.mod.*;
+import tranqol.content.*;
+
+public class TranQOL extends Mod{
+
+    public TranQOL(){}
+
+    @Override
+    public void loadContent(){
+        TranQOLBlocks.load();
+    }
+}
---
+++ src/tranqol/content/TranQOLBlocks.java
@@ -0,0 +1,40 @@
+package tranqol.content;
+
+import mindustry.world.*;
+
+public class TranQOLBlocks{
+    public static Block
+
+    // region liquid
+
+    liquidOverflowGate, liquidUnderflowGate,
+
+    // region liquid - Erekir
+
+    reinforcedLiquidOverflowGate, reinforcedLiquidUnderflowGate,
+
+    // endregion
+    // region power - Erekir
+
+    beamDiode;
+
+    // endregion
+
+    public static void load(){
+        // region liquid
+
+        //TODO
+
+        // endregion
+        // region liquid - Erekir
+
+        //TODO
+
+        // endregion
+        // region power -
+
+        //TODO
+
+        //endregion
+    }
+}
