ab10a5d18b719f5ad3cc3b207266237f2dca4f3a
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-05-11 13:27:11 -0700

    Remove phase costs
    Really dumb

--- src/tranqol/content/TranqolBlocks.java
+++ src/tranqol/content/TranqolBlocks.java
@@ -107,7 +107,7 @@ public class TranqolBlocks{
         // region payload - Serpulo
 
         payloadJunction = new PayloadJunction("payload-junction"){{
-            requirements(Category.units, with(Items.graphite, 10, Items.copper, 15, Items.phaseFabric, 5));
+            requirements(Category.units, with(Items.graphite, 15, Items.copper, 20));
             canOverdrive = false;
             payloadLimit = 2f;
             hideDetails = false;
@@ -117,7 +117,7 @@ public class TranqolBlocks{
         // region payload - Erekir
 
         reinforcedPayloadJunction = new PayloadJunction("reinforced-payload-junction"){{
-            requirements(Category.units, with(Items.tungsten, 10, Items.beryllium, 5, Items.phaseFabric, 5));
+            requirements(Category.units, with(Items.tungsten, 15, Items.beryllium, 10));
             moveTime = 35f;
             canOverdrive = false;
             payloadLimit = 2f;
